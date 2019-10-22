8f7820bd7226aedaf2baa55f5a1860d8d40ecc9b
Dino Pehratovic <dino.pehratovic@outlook.de>
2019-10-22 19:16:52 +0200

    Updated variable names and added more console input to show which steps were made

--- src/patch_file_creator.py
+++ src/patch_file_creator.py
@@ -10,24 +10,28 @@ import re
 supported_tools = ["diff", "bsdiff", "xdelta", "rsync"]
 
 
-def create_patch_directory_structure(destination_directory, patch_directory):
-    for root, dirs, _ in os.walk(destination_directory):
-        root = root.replace(destination_directory, "")
-        os.makedirs(patch_directory + "/" + root)
+def create_patch_directory_structure(modified_directory, patch_directory):
+    for root, dirs, _ in os.walk(modified_directory):
+        root = root.replace(modified_directory, "")
+        if not os.path.exists(patch_directory + "/" + root):
+            path = patch_directory + "/" + root
+            print("New directory created".format(path))
+            os.makedirs(path)
 
 
-def move_new_files_into_patch_directory(source_path, destination_path, file_list):
+def copy_new_files_into_patch_directory(from_path, to_path, file_list):
     for file in file_list:
-        shutil.copy(source_path + file[1] + file[0], destination_path + file[1] + file[0])
+        shutil.copy(from_path + file[1] + file[0], to_path + file[1] + file[0])
 
 
-def create_diff_files(file_list, origin_path, modified_path, patch_path):
+def create_diff_files(file_list, original_version_path, modified_version_path, patch_path):
+    #TODO: Implement diff creation for all other differential update tools
     for file in file_list:
         print("Creating diff for {}".format(file[0]))
-        original_file = origin_path + "/" + file[1] + "/" + file[0]
-        modified_file = modified_path + "/" + file[1] + "/" + file[0]
+        original_version_file = original_version_path + "/" + file[1] + "/" + file[0]
+        modified_version_file = modified_version_path + "/" + file[1] + "/" + file[0]
         patch_file = patch_path + "/" + file[1] + "/" + file[0]
-        subprocess.call(["bsdiff", original_file, modified_file, patch_file])
+        subprocess.call(["bsdiff", original_version_file, modified_version_file, patch_file])
 
 
 def is_path_valid(path_to_check):
@@ -49,24 +53,24 @@ def fill_list_with_searched_values(file_list_without_paths, file_list_with_paths
     return result_list
 
 
-def detect_all_new_and_modified_files(source_list, destination_list):
+def detect_all_new_and_modified_files(original_version_file_list, modified_version_file_list):
     new_files = []
     modified_files = []
 
-    source_files = [ file_and_path[0] for file_and_path in source_list]
-    destination_files = [file_and_path[0] for file_and_path in destination_list]
+    original_version_files = [ file_and_path[0] for file_and_path in original_version_file_list]
+    modified_version_files = [file_and_path[0] for file_and_path in modified_version_file_list]
 
-    for dest_file in destination_files:
-        if dest_file not in source_files:
-            new_files.append(dest_file)
+    for modified_version_file in modified_version_files:
+        if modified_version_file not in original_version_files:
+            new_files.append(modified_version_file)
         else:
-            modified_files.append(dest_file)
+            modified_files.append(modified_version_file)
 
     new_files_with_paths = fill_list_with_searched_values(file_list_without_paths=new_files,
-                                                          file_list_with_paths=destination_list)
+                                                          file_list_with_paths=modified_version_file_list)
 
     modified_files_with_paths = fill_list_with_searched_values(file_list_without_paths=modified_files,
-                                                               file_list_with_paths=destination_list)
+                                                               file_list_with_paths=modified_version_file_list)
     
     return modified_files_with_paths, new_files_with_paths
 
@@ -83,85 +87,94 @@ def iterate_through_directory(directory_path):
 
 
 def check_arguments():
+    #TODO: Add -f flag for single file diffing. Currently, the program will only execute correctly for directories!
+    #TODO: Make the program work for archives 
     parser = argparse.ArgumentParser()
-    parser.add_argument("-s", "--source_path", 
+    parser.add_argument("-o", "--original_version_path", 
                         help="This path contains the original version of the directory/file/archive")
-    parser.add_argument("-d", "--destination_path", 
+    parser.add_argument("-m", "--modified_version_path", 
                         help="This path contains the modified version of the directory/file/archive")
-    parser.add_argument("-p", "--patch_file_path", 
+    parser.add_argument("-p", "--patch_path", 
                         help="The created diff file will be saved within this path")
     parser.add_argument("-t", "--tool", 
                         help="Choose one of the supported differential update tools listed below")
     args = parser.parse_args()
 
-    if not is_path_valid(args.source_path):
+    if not is_path_valid(args.original_version_path):
         exit(1)
-    if not is_path_valid(args.destination_path):
+    if not is_path_valid(args.modified_version_path):
         exit(1)
     if args.tool not in supported_tools:
         print("Chosen tool is not supported by this program! Processing stops!")
         exit(2)
     else:
-        print("All needed paths exist. Processing continues!")
         print("{} was chosen as the differential update tool for processing!".format(args.tool))
 
-    return args.source_path, args.destination_path, args.patch_file_path, args.tool
+    return args.original_version_path, args.modified_version_path, args.patch_path, args.tool
 
 
 if __name__ == "__main__":
     # Step 1: Check if all paths and arguments are valid
-    source_path, destination_path, patch_path, chosen_tool = check_arguments()
+    print("Checking if the paths of the original and modified versions are valid and exist!")
+    original_version_path, modified_version_path, patch_path, chosen_tool = check_arguments()
+    print("All needed paths are valid and exist. Processing continues")
 
-    # Step 2: Iterate through source and destination directories and search for all files
+    # Step 2: Iterate through the original and modified directories and search for all differences
     #         Note: Files with their corresponding paths will be saved
-    print("Starting to iterate through the source path!")
-    source_file_list = iterate_through_directory(directory_path=source_path)
+    print("Starting to iterate through the original version!")
+    original_version_file_list = iterate_through_directory(directory_path=original_version_path)
     print("Result: ")
-    print(source_file_list)
+    print(original_version_file_list)
     print("=======================================================================")
 
-    print("Starting to iterate through the destination path!")
-    destination_file_list = iterate_through_directory(directory_path=destination_path)
+    print("Starting to iterate through the modified version!")
+    modified_version_file_list = iterate_through_directory(directory_path=modified_version_path)
     print("Result: ")
-    print(destination_file_list)
+    print(modified_version_file_list)
     print("=======================================================================")
 
     # Step 3: Create structure of the patch-directory
-    create_patch_directory_structure(destination_directory=destination_path, patch_directory=patch_path)
+    print("Starting to create patch directory structure!")
+    create_patch_directory_structure(modified_directory=modified_version_path, patch_directory=patch_path)
+    print("Finished creating patch directory structure!")
 
     # Step 4: Go through destination list and create a patch file for each file in this list
     #         which was modified
     #         Note: This time it is necessary to measure the time which is needed 
     #               to create the diff for all files
-    '''
-    modified list contains of the following aspects: 
-        1. location of the old version
-        2. location of the new version
-    '''
-    modified_files, new_files = detect_all_new_and_modified_files(source_list=source_file_list, destination_list=destination_file_list)
+    print("Starting to detect all new and modified files!")
+    modified_files, new_files = detect_all_new_and_modified_files(original_version_file_list=original_version_file_list, 
+                                                                  modified_version_file_list=modified_version_file_list)
+    print("List of all files which were added within the modified version: ")
+    print(new_files)
+    print("List of all files which were modified within the modified version: ")
+    print(modified_files)
 
+    print("Creating diff-files for all modified files!")
     start_time = time.time()
-    create_diff_files(file_list=modified_files, origin_path=source_path, modified_path=destination_path, patch_path=patch_path)
+    create_diff_files(file_list=modified_files, original_version_path=original_version_path, 
+                      modified_version_path=modified_version_path, patch_path=patch_path)
     end_time = time.time()
+    print("All diff-files were created and stored within the patch directory!")
     # Calculates time which was needed to create all diff-files
     needed_time = end_time - start_time
+    print("Time needed to create all diff-files: {}".format(needed_time))
 
     # Step 5: Move all new files into their corresponding 
     #           location within the patch-directory
-    move_new_files_into_patch_directory(source_path=destination_path, destination_path=patch_path, file_list=new_files)
+    print("Copying all new files from the modified version into the patch directory!")
+    copy_new_files_into_patch_directory(from_path=modified_version_path, to_path=patch_path, file_list=new_files)
+    print("Finished copying process!")
 
     # Step 6: Create JSON file and save all measurable stats within it
+    print("Saving JSON-file with all relevant information as: stats.json")
     stats = {
         "time_needed_to_create_patch_file": needed_time,
         "time_needed_to_apply_patch_file": "",
-        "size_of_the_source": "",
-        "size_of_the_destination": "",
-        "original_size_vs_patch_file_size": ""
+        "size_of_the_original_version": "",
+        "size_of_the_modified_version": "",
+        "original_size_vs_patch_size": ""
     }
     #TODO: Save created JSON-file
-    #TODO: Rename source to origin 
-    #TODO: Rename destination to modified
-    #TODO: Consider renaming patch to destination
-
+    print("JSON-file successfully saved!")
     print("Finished processing!")
-    #shutil.rmtree(patch_path)
